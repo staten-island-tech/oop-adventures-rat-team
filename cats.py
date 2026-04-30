adaee51a2242eb1da6f5932cb7600d26d5d52089
Unixprime7 <michaeld801@nycstudents.net>
2026-04-30 09:15:04 -0400

    Update cats.py

--- cats.py
+++ cats.py
@@ -1,2 +1,11 @@
+import random 
 class cat:
-    def __init__(self, health, power, speed, )
+    def __init__(self, health, power, speed):
+        self.health = health
+        self.power = power
+        self.speed = speed
+    def attack(self):
+
+
+
+wild_cat = cat(random.randint(25, 45), random.randint(10 , 20), 10)
