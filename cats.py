2e6a02458c2d1551e8b6d40bd4cc09cd39cc09c5
Unixprime7 <michaeld801@nycstudents.net>
2026-04-30 09:25:30 -0400

    Update cats.py

--- cats.py
+++ cats.py
@@ -1,11 +1,52 @@
 import random 
+import pygame
 class cat:
     def __init__(self, health, power, speed):
         self.health = health
         self.power = power
         self.speed = speed
-    def attack(self):
+    def attack(self, rat):
+        rat.health -= self.power
 
 
 
 wild_cat = cat(random.randint(25, 45), random.randint(10 , 20), 10)
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 3, screen.get_height() / 2)
+
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("light blue")
+
+    pygame.draw.circle(screen, "red", player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 600 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 600 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 600 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 600 * dt
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
