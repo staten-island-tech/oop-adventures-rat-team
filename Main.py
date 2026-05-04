de9a228c856a45ac9ff7d3ffd86483ee9b47cf9b
EvaYaroslav12 <eva.yaroslavskiy@gmail.com>
2026-05-03 21:27:33 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
-dt = 0
+dt = 1
 
 player_pos = pygame.Vector2(screen.get_width() / 3, screen.get_height() / 2)
 
@@ -16,9 +16,10 @@ while running:
             running = False
 
     screen.fill("light blue")
-    # my_image = pygame.image.load("RatRightSprite")
+    my_image = pygame.image.load("RatRightSprite.png")
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    # pygame.draw.circle(screen, "red", player_pos, 40)
+    screen.blit(my_image, player_pos)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
@@ -26,9 +27,9 @@ while running:
     if keys[pygame.K_s]:
         player_pos.y += 600 * dt
     if keys[pygame.K_a]:
-        player_pos.x -= 600 * dt
+        player_pos.x -= 600 * dt 
     if keys[pygame.K_d]:
-        player_pos.x += 600 * dt
+        player_pos.x += 600 * dt 
 
     pygame.display.flip()
 
