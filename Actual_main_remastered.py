ce75388d4459f0eaa6a422fec5acc0d7c5ba2c5c
EvaYaroslav12 <eva.yaroslavskiy@gmail.com>
2026-05-19 09:17:40 -0400

    fixed cuz micheal a bum

--- Actual_main_remastered.py
+++ Actual_main_remastered.py
@@ -63,6 +63,14 @@ while running:
     if (img_flip == True): 
         my_image = pygame.transform.flip(my_image, True, False)
 
+    left_wall = pygame.draw.rect(screen, (255, 0, 0), (0, 0, 60, 900))
+    right_wall = pygame.draw.rect(screen, (255, 0, 0), (1220, 0, 60, 900))
+    top_wall = pygame.draw.rect(screen, (255, 0, 0), (0, 0, 1220, 60))
+    bottom_wall = pygame.draw.rect(screen, (255, 0, 0), (0, 660, 1220, 60))
+    screen.blit(my_image, (player_pos.x - my_image.get_width()/2, player_pos.y - my_image.get_height()/2))
+
+    
+
     cat_keys = pygame.key.get_pressed()
 
     if cat_keys[pygame.K_e]:
@@ -82,6 +90,9 @@ while running:
     
     origx = player_pos.x
     origy = player_pos.y
+    hitboxx = player_pos.x
+    hitboxy = player_pos.y
+    hb = 50
     
 
     keys = pygame.key.get_pressed()
@@ -89,24 +100,28 @@ while running:
         player_pos.y -= 600 * dt
         img_rotation = 90
         img_flip = False
+        hitboxy = player_pos.y - hb
 
 
     if keys[pygame.K_s]:
         player_pos.y += 600 * dt
         img_rotation = 270
         img_flip = False
+        hitboxy = player_pos.y + hb
         
 
     if keys[pygame.K_a]:
         player_pos.x -= 600 * dt
         img_rotation = 0
         img_flip = True
+        hitboxx = player_pos.x - hb
         
 
     if keys[pygame.K_d]:
         player_pos.x += 600 * dt
         img_rotation = 0
         img_flip = False
+        hitboxx = player_pos.x + hb
 
     if keys[pygame.K_LSHIFT]:
          sprint = 2
@@ -114,26 +129,32 @@ while running:
     else:
         sprint = 1
 
-    player_rect = my_image.get_rect(center=(player_pos.x, player_pos.y))
+    radius = 40
+    my_image = pygame.Surface((radius * 2, radius * 2), pygame.SRCALPHA)
+
+    pygame.draw.circle(my_image, (0, 0, 255), (radius, radius), radius)
+
+    player_rect = my_image.get_rect(center=(hitboxx, hitboxy))
+    # pygame.draw.rect(screen, (0,0,255), player_rect)
 
     if player_rect.colliderect(left_wall):
         #player_rect.left = left_wall.right
         player_pos.x = origx
-        player_pos.y = origy
+        # player_pos.y = origy
 
     if player_rect.colliderect(right_wall):
         #player_rect.right = right_wall.left
         player_pos.x = origx
-        player_pos.y = origy
+        # player_pos.y = origy
 
     if player_rect.colliderect(top_wall):
         #player_rect.top = top_wall.bottom
-        player_pos.x = origx
+        # player_pos.x = origx
         player_pos.y = origy
 
     if player_rect.colliderect(bottom_wall):
         #player_rect.bottom = bottom_wall.top
-        player_pos.x = origx
+        # player_pos.x = origx
         player_pos.y = origy
 
 
@@ -217,5 +238,4 @@ while running:
     # dt is delta time in seconds since last frame, used for framerate-
     # independent physics.
     dt = clock.tick(60) / 1000 * sprint
-    print(img_flip)
 pygame.quit()
