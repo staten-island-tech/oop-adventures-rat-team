2bf58766fb0c7823808338e30f36de63d59dd43f
EvaYaroslav12 <eva.yaroslavskiy@gmail.com>
2026-05-20 09:02:53 -0400

    lil changes

--- loading_screen.py
+++ loading_screen.py
@@ -7,7 +7,7 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 pygame.font.init()
-gamerune = False
+gamerun = False
 
 dance_gif = gif_pygame.load("rat-dance_gif.gif") 
 
@@ -19,20 +19,21 @@ while running:
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             if start_button.collidepoint(event.pos):
-                print("Button clicked!")
-                gamerune = True
-            
-    screen.fill("light blue")
-    dance_gif.render(screen, (800, 200))
+                print("Game Started!")
+                gamerun = True
 
-    title_font = pygame.font.SysFont('Arial', 60, bold = True) 
-    title = title_font.render('RAT RUNNER', True, (0,0,0))
-    screen.blit(title, (500, 10))
+    if gamerun == False:
+        screen.fill("light blue")
+        dance_gif.render(screen, (800, 200))
 
-    start_font = pygame.font.SysFont('Arial', 30, bold = True) 
-    start_button = pygame.draw.rect(screen, (100, 100, 255), (50, 200, 300, 100))
-    start_button_text = start_font.render('START', True, (255, 255, 255))
-    screen.blit(start_button_text, (150, 230))
+        title_font = pygame.font.SysFont('Arial', 60, bold = True) 
+        title = title_font.render('RAT RUNNER', True, (0,0,0))
+        screen.blit(title, (500, 10))
+
+        start_font = pygame.font.SysFont('Arial', 30, bold = True) 
+        start_button = pygame.draw.rect(screen, (100, 100, 255), (50, 200, 300, 100))
+        start_button_text = start_font.render('START', True, (255, 255, 255))
+        screen.blit(start_button_text, (150, 230))
 
         
 
