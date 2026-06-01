21564d2f73b8012367c647fc8b3598981887117a
EvaYaroslav12 <eva.yaroslavskiy@gmail.com>
2026-06-01 15:23:53 -0400

    more gifs + ads

--- adds.py
+++ adds.py
@@ -12,9 +12,12 @@ def pls_wait_30secs():
     gamerun = False
     import random
     pygame.font.init()
-    ads = ['rat-dance_gif.gif', 'ratating_rat_gif.gif', 'ratmunchy_gif.gif']
+    ads = ['boogierat.gif', 'hoprat.gif', 'pushuprat.gif', 'rat-dancebackrooms.gif', 'wtf_rat.gif', 'ratating_rat_gif.gif', 'ratmunchy_gif.gif']
     bg_image = pygame.image.load("starting_background.jpg")
     bg_image = pygame.transform.scale(bg_image, (1280, 720))
+    ad = ads[random.randint(0,6)]
+    ad_file = random.choice(ads)
+    ad = gif_pygame.load(ad_file)
 
     while True:
 
@@ -30,29 +33,30 @@ def pls_wait_30secs():
                     return True
 
         if gamerun == False:
-            screen.fill("light blue")
-            screen.blit(bg_image, (0, 0))
-            dance_gif.render(screen, (780, 265))
+            screen.fill("white")
+            # screen.blit(bg_image, (0, 0))
+            ad.render(screen, (300, 265))
 
-            title_font = pygame.font.SysFont('Arial', 60, bold = True) 
-            title = title_font.render(ads[random.randint(1,3)], True, (255,255,255))
-            screen.blit(title, (500, 10))
+            title_font = pygame.font.SysFont('Arial', 40, bold = True) 
+            title = title_font.render('your game will resume shortly after this add...', True, (0,0,0))
+            screen.blit(title, (250, 10))
 
             start_font = pygame.font.SysFont('Arial', 30, bold = True) 
-            start_button = pygame.draw.rect(screen, (100, 100, 255), (50, 200, 300, 100))
-            start_button_text = start_font.render('START', True, (255, 255, 255))
-            screen.blit(start_button_text, (150, 230))
+            start_button = pygame.draw.rect(screen, (100, 100, 255), (1200, 10, 50, 50))
+            start_button_text = start_font.render('X', True, (0, 0, 0))
+            screen.blit(start_button_text, (1210, 10))
 
         if start_button.collidepoint(pygame.mouse.get_pos()):
             # print("Hovering!")
             # removal_button = pygame.draw.rect(screen, (173, 216, 230), (50, 200, 300, 100))
-            screen.blit(bg_image, (0, 0))
-            start_button = pygame.draw.rect(screen, (140, 140, 255), (55, 205, 290, 90))
-            dance_gif.render(screen, (780, 265))
-            start_button_text = start_font.render('START', True, (0, 0, 0))
+            # screen.blit(bg_image, (0, 0))
+            screen.fill('white')
+            start_button = pygame.draw.rect(screen, (100, 100, 255), (800, 0, 100, 100))
+            ad.render(screen, (300, 265))
+            start_button_text = start_font.render('X', True, (0, 0, 0))
             screen.blit(start_button_text, (150, 230))
-            screen.blit(title, (500, 10))
-            screen.blit(start_button_text, (150, 230))
+            screen.blit(title, (250, 10))
+            screen.blit(start_button_text, (810, 110))
 
 
             
